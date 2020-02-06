d4998620b26d73f22b301aac888261768c9a6d14
Jeremy Nuttall <jtnuttal@usc.edu>
2020-02-05 21:29:22 -0800

    New frontend things

---
+++ staticfiles/assets/TankBattle/precache-manifest.b860eba8ac365c10aece07be7676d072.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a7e22c51ee08c20ece243c028469a6",
+    "url": "/assets/sheet.png"
+  },
+  {
+    "revision": "16806a77bb26a1c947d1a3ad8e780105",
+    "url": "/assets/sheet1.png"
+  },
+  {
+    "revision": "9de22b7642ca57bb50efdd39921cfe85",
+    "url": "/assets/sheet2.png"
+  },
+  {
+    "revision": "eaff852723a6e9886d6fd063d27cb611",
+    "url": "/assets/tank/body.png"
+  },
+  {
+    "revision": "5ad273b9f8d48fff12c0987719e50e2c",
+    "url": "/assets/tank/gun.png"
+  },
+  {
+    "revision": "945d1425fd7f74628848cc070b71d574",
+    "url": "/assets/tankOnly.png"
+  },
+  {
+    "revision": "cb2580743354e77e412e726608c3332a",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "838501f8c9d9abdffc5f7e75e1c25ff0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f695dab5560b11197329574f8986c0f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "5c63221966c82be15a9c",
+    "url": "/static/css/main.724d3f00.chunk.css"
+  },
+  {
+    "revision": "5c63221966c82be15a9c",
+    "url": "/static/js/main.5c632219.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "604c6712e7dcf2851e2c",
+    "url": "/static/js/vendors~main.604c6712.chunk.js"
+  }
+]);
