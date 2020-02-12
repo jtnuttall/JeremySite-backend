e7f3619f82e7525525e494bcf0c6028040ce0d0b
Jeremy Nuttall <jtnuttal@usc.edu>
2020-02-12 09:55:41 -0800

    Add projects list preliminary

--- API/views.py
+++ API/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from rest_framework import views
 from rest_framework import mixins
 from rest_framework import generics
+from rest_framework.response import Response
 from .models import *
 from .serializers import *
+import sys
 
 class ExperienceList(mixins.ListModelMixin, generics.GenericAPIView):
 	queryset = Experience.objects.all()
@@ -18,16 +21,29 @@ class ExperienceDetails(mixins.RetrieveModelMixin, generics.GenericAPIView):
 	def get(self, request, *args, **kwargs):
 		return self.retrieve(request, *args, **kwargs)
 
-class ProjectList(mixins.ListModelMixin, generics.GenericAPIView):
-	queryset = Project.objects.all()
-	serializer_class = ProjectSerializer
-
+class ProjectList(views.APIView):
 	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
+		return self.post(request, *args, **kwargs)
 
-class ProjectDetails(mixins.RetrieveModelMixin, generics.GenericAPIView):
-	queryset = Project.objects.all()
-	serializer_class = ProjectSerializer
+	def post(self, request, *args, **kwargs):
+		projects = Project.objects.order_by('priority')
 
+		for project in projects:
+			project.language = project.get_language_display()
+			project.framework = project.get_framework_display()
+
+		serializer = ProjectSerializer(projects, many=True)
+		return Response(serializer.data)
+
+class ProjectDetails(views.APIView):
 	def get(self, request, *args, **kwargs):
-		return self.retrieve(request, *args, **kwargs)
+		return self.post(request, *args, **kwargs)
+
+	def post(self, request, *args, **kwargs):
+		project = Project.objects.get(pk=kwargs['pk'])
+
+		project.language = project.get_language_display()
+		project.framework = project.get_framework_display()
+
+		serializer = ProjectSerializer(project, many=False)
+		return Response(serializer.data)
